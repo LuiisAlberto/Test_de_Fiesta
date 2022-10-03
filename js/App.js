8ad0484bd5c4a54107d7f47dd72f1d831eaa6f66
LuiisAlberto <rayomp263@gmail.com>
2022-10-03 12:42:10 -0500

    Test Terminado

--- js/App.js
+++ js/App.js
@@ -6,29 +6,29 @@ var fiestas=[
 
 var preguntas=[
 	new Pregunta("¿Con qué frecuencia vas o realizas fiestas?",[
-		new Respuesta("Cada mes.","assets/img/pre1_res1.jpeg",[0,1,0]),
-		new Respuesta("Cada fin de semana, siempre.","assets/img/pre1_res2.jpg",[1,0,0]),
-		new Respuesta("En ocasiones especiales.","assets/img/pre1_res3.jpg",[0,1,0])
+		new Respuesta("Cada mes.","assets/img/cada_mes.jpeg",[0,1,0] ),
+		new Respuesta("Cada fin de semana, siempre.","assets/img/todos_los_fines.jpeg",[1,0,0]),
+		new Respuesta("En ocasiones especiales.","assets/img/ocasiones_especiales.jpeg",[0,1,0])
 	]),
 	new Pregunta("¿Qué ropa sueles llevar?",[
-		new Respuesta("Ropa un poco formal.","assets/img/pre2_res1.jpg",[0,1,0]),
-		new Respuesta("Algo cómodo.","assets/img/pre2_res2.jpg",[0,0,1]),
-		new Respuesta("Lo que lleve puesto en ese momento.","assets/img/pre2_res3.jpg",[1,0,0])
+		new Respuesta("Ropa un poco formal.","assets/img/formal.jpg",[0,1,0]),
+		new Respuesta("Algo cómodo.","assets/img/comodo.jpeg",[0,0,1]),
+		new Respuesta("Lo que lleve puesto en ese momento.","assets/img/lo_primero.jpeg",[1,0,0])
 	]),
 	new Pregunta("¿Con qué bebida te identificas más?",[
-		new Respuesta("Cualquier bebida que lleve alcohol.","assets/img/pre3_res1.jpg",[1,0,0]),
-		new Respuesta("Un buen vino.","assets/img/pre3_res2.jpg",[0,1,0]),
-		new Respuesta("Un cóctel.","assets/img/pre3_res3.jpg",[0,0,1])
+		new Respuesta("Cualquier bebida que lleve alcohol.","assets/img/cualquier_bebida.jpg",[1,0,0]),
+		new Respuesta("Un buen vino.","assets/img/vino.jpg",[0,1,0]),
+		new Respuesta("Un cóctel.","assets/img/coctel.jpg",[0,0,1])
 	]),
 	new Pregunta("¿A dónde te gusta más salir?",[
-		new Respuesta("Grandes eventos.","assets/img/pre4_res1.jpg",[1,0,0]),
-		new Respuesta("Pequeños locales.","assets/img/pre4_res2.jpg",[0,0,1]),
-		new Respuesta("Casas de otros.","assets/img/pre4_res4.jpg",[1,0,0])
+		new Respuesta("Grandes eventos.","assets/img/grandes_eventos.jpeg",[1,0,0]),
+		new Respuesta("Pequeños locales.","assets/img/pequeños_locales.jpeg",[0,0,1]),
+		new Respuesta("Casas de otros.","assets/img/casas_de_otros.jpeg",[1,0,0])
 	]),
 	new Pregunta("Si hay alcohol en una fiesta ¿Cuánto bebes?",[
-		new Respuesta("Nada, me empedo con olerlo.","assets/img/pre5_res1.jpg",[0,1,1]),
-		new Respuesta("Hasta que se acabe, y voy por más.","assets/img/pre5_res2.jpg",[2,0,0]),
-		new Respuesta("Hasta que se terminte, pero no compro.","assets/img/pre5_res3.jpg",[1,0,0]),
+		new Respuesta("Nada, me empedo con olerlo.","assets/img/solo_olerlo.jpeg",[0,1,1]),
+		new Respuesta("Hasta que se acabe, y voy por más.","assets/img/voy_por_mas.jpeg",[2,0,0]),
+		new Respuesta("Hasta que se terminte, pero no compro.","assets/img/no_compro.jpeg",[1,0,0]),
 	])
 ];
 
